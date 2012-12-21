0e1947ca8da62713d3730006d594c4ec50b35e81
Michael Kane <michael.kane@codegent.net>
2012-12-21 18:48:29 +0000

    Starting with some bootstrap

---
+++ app.py
@@ -0,0 +1,74 @@
+import os
+from functools import wraps
+from flask import *
+import settings
+import logging
+
+app = Flask(__name__)
+app.secret_key = settings.SECRET_KEY
+app.config.from_object('settings')
+
+
+########################
+#        VIEWS         #
+########################
+
+
+###### Basic Setup ######
+
+@app.after_request
+def add_header(response):
+    """Add header to force latest IE rendering engine and Chrome Frame."""
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    return response
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_error(error):
+    """Custom 500 page."""
+    return render_template('500.html'), 500
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=cache_buster_url_for)
+
+def cache_buster_url_for(endpoint, **values):
+    if endpoint == 'static':
+        values['v'] = settings.STATIC_RESOURCE_VERSION
+    return url_for(endpoint, **values)
+
+
+###### Helpers ######
+
+def redirect_back_internal_url():
+    """
+    Get a url to send the user back to previous page
+
+    If they would be sent to an off-site page, return the url for the
+    homepage
+    """
+    from urlparse import urlparse
+    if request.referrer:
+        referrer_netloc = urlparse(request.referrer).netloc
+        app_netloc = urlparse(request.url_root).netloc
+        if referrer_netloc == app_netloc:
+            return request.referrer
+    return url_for('home')
+
+
+###### Pages ######
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    host = str(os.environ.get('HOST', '0.0.0.0'))
+    app.run(host=host, port=port, debug=True)
